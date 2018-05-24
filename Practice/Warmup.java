4af6fad7014f095d9b057c333751dfdb1abc305d
lrliang <lrliang@CNlrliang.local>
2018-05-24 22:41:17 +0800

    method countXX done

--- Practice/Warmup.java
+++ Practice/Warmup.java
@@ -1,5 +1,5 @@
 public class Warmup {
-    public String stringTimes(String str, int n) {
+    String stringTimes(String str, int n) {
         StringBuffer result = new StringBuffer();
         for (int i = 0; i < n; i++) {
             result = result.append(str);
@@ -7,9 +7,17 @@ public class Warmup {
         return String.valueOf(result);
     }
 
-    public String frontTimes(String str, int n) {
+    String frontTimes(String str, int n) {
         String frontStr = str.length() > 3 ? str.substring(0, 3) : str;
         return stringTimes(frontStr, n);
+    }
+
+    int countXX(String str) {
+        int xCount = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equals("xx")) xCount++;
+        }
+        return xCount;
     }
 
 }
