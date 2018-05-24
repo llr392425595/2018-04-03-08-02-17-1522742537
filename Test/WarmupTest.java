49c9144b86dceafd6095c138779e810bae535250
lrliang <lrliang@CNlrliang.local>
2018-05-24 21:45:01 +0800

    add test case fronttimes

--- Test/WarmupTest.java
+++ Test/WarmupTest.java
@@ -29,4 +29,15 @@ public class WarmupTest {
         assertThat(warmupPractice.stringTimes("code", 2),is("codecode"));
         assertThat(warmupPractice.stringTimes("code", 3),is("codecodecode"));
     }
+
+    @Test
+    public void frontTimes() {
+        assertThat(warmupPractice.frontTimes("Chocolate", 2),is("ChoCho"));
+        assertThat(warmupPractice.frontTimes("Chocolate", 3),is("ChoChoCho"));
+        assertThat(warmupPractice.frontTimes("Abc", 3),is("AbcAbcAbc"));
+        assertThat(warmupPractice.frontTimes("Ab", 4),is("AbAbAbAb"));
+        assertThat(warmupPractice.frontTimes("A", 4),is("AAAA"));
+        assertThat(warmupPractice.frontTimes("", 4),is(""));
+        assertThat(warmupPractice.frontTimes("Abc", 0),is(""));
+    }
 }
