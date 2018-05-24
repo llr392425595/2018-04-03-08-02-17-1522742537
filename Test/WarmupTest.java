a52a4fd017b5b988dd5ecffc148a62ff40845789
lrliang <lrliang@CNlrliang.local>
2018-05-24 21:26:00 +0800

    update .gitignore add dir output

---
+++ Test/WarmupTest.java
@@ -0,0 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class WarmupTest {
+    private Warmup warmupPractice;
+
+    @Before
+    public void setUp() throws Exception {
+        warmupPractice = new Warmup();
+    }
+
+    @Test
+    public void stringTimes() {
+        String actual = warmupPractice.stringTimes("Hi", 2);
+        String expected = "HiHi";
+
+        assertThat(actual,is(expected));
+
+        assertThat(warmupPractice.stringTimes("Hi", 3),is("HiHiHi"));
+        assertThat(warmupPractice.stringTimes("Hi", 1),is("Hi"));
+        assertThat(warmupPractice.stringTimes("Hi", 0),is(""));
+        assertThat(warmupPractice.stringTimes("Hi", 5),is("HiHiHiHiHi"));
+        assertThat(warmupPractice.stringTimes("Oh Boy!", 2),is("Oh Boy!Oh Boy!"));
+        assertThat(warmupPractice.stringTimes("x", 4),is("xxxx"));
+        assertThat(warmupPractice.stringTimes("", 4),is(""));
+        assertThat(warmupPractice.stringTimes("code", 2),is("codecode"));
+        assertThat(warmupPractice.stringTimes("code", 3),is("codecodecode"));
+    }
+}
