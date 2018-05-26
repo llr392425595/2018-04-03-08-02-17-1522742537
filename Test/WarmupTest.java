ba0ac58389655d3c570d651ea82e2955d35c42a2
llr392425595 <392425595@qq.com>
2018-05-26 16:32:27 +0800

    add some method test cases

--- Test/WarmupTest.java
+++ Test/WarmupTest.java
@@ -96,4 +96,162 @@ public class WarmupTest {
         assertThat(warmupPractice.stringSplosion("Good"),is("GGoGooGood"));
         assertThat(warmupPractice.stringSplosion("Bad"),is("BBaBad"));
     }
+
+    @Test
+    public void last2() {
+        assertThat(warmupPractice.last2("hixxhi"),is(1));
+        assertThat(warmupPractice.last2("xaxxaxaxx"),is(1));
+        assertThat(warmupPractice.last2("axxxaaxx"),is(2));
+        assertThat(warmupPractice.last2("xxaxxaxxaxx"),is(3));
+        assertThat(warmupPractice.last2("xaxaxaxx"),is(0));
+        assertThat(warmupPractice.last2("xxxx"),is(2));
+        assertThat(warmupPractice.last2("13121312"),is(1));
+        assertThat(warmupPractice.last2("11212"),is(1));
+        assertThat(warmupPractice.last2("13121311"),is(0));
+        assertThat(warmupPractice.last2("1717171"),is(2));
+        assertThat(warmupPractice.last2("hi"),is(0));
+        assertThat(warmupPractice.last2("h"),is(0));
+        assertThat(warmupPractice.last2(""),is(0));
+    }
+
+    @Test
+    public void arryCount9() {
+        assertThat(warmupPractice.arrayCount9(new int[]{1,2,9}),is(1));
+        assertThat(warmupPractice.arrayCount9(new int[]{1,9,9}),is(2));
+        assertThat(warmupPractice.arrayCount9(new int[]{1,9,9,3,9}),is(3));
+        assertThat(warmupPractice.arrayCount9(new int[]{1,2,3}),is(0));
+        assertThat(warmupPractice.arrayCount9(new int[]{}),is(0));
+        assertThat(warmupPractice.arrayCount9(new int[]{4, 2, 4, 3, 1}),is(0));
+        assertThat(warmupPractice.arrayCount9(new int[]{9, 2, 4, 3, 1}),is(1));
+    }
+
+    @Test
+    public void arrayFront9() {
+        assertThat(warmupPractice.arrayFront9(new int[]{1, 2, 9, 3, 4}),is(true));
+        assertThat(warmupPractice.arrayFront9(new int[]{1, 2, 3, 4, 9}),is(false));
+        assertThat(warmupPractice.arrayFront9(new int[]{1, 2, 3, 4, 5}),is(false));
+        assertThat(warmupPractice.arrayFront9(new int[]{9, 2, 3}),is(true));
+        assertThat(warmupPractice.arrayFront9(new int[]{1, 9, 9}),is(true));
+        assertThat(warmupPractice.arrayFront9(new int[]{1, 2, 3}),is(false));
+        assertThat(warmupPractice.arrayFront9(new int[]{1, 9}),is(true));
+        assertThat(warmupPractice.arrayFront9(new int[]{5, 5}),is(false));
+        assertThat(warmupPractice.arrayFront9(new int[]{2}),is(false));
+        assertThat(warmupPractice.arrayFront9(new int[]{9}),is(true));
+        assertThat(warmupPractice.arrayFront9(new int[]{}),is(false));
+        assertThat(warmupPractice.arrayFront9(new int[]{3, 9, 2, 3, 3}),is(true));
+    }
+
+    @Test
+    public void array123() {
+        assertThat(warmupPractice.array123(new int[]{1, 1, 2, 3, 1}), is(true));
+        assertThat(warmupPractice.array123(new int[]{1, 1, 2, 4, 1}), is(false));
+        assertThat(warmupPractice.array123(new int[]{1, 1, 2, 1, 2, 3}), is(true));
+        assertThat(warmupPractice.array123(new int[]{1, 1, 2, 1, 2, 1}), is(false));
+        assertThat(warmupPractice.array123(new int[]{1, 2, 3, 1, 2, 3}), is(true));
+        assertThat(warmupPractice.array123(new int[]{1, 2, 3}), is(true));
+        assertThat(warmupPractice.array123(new int[]{1, 1, 1}), is(false));
+        assertThat(warmupPractice.array123(new int[]{1, 2}), is(false));
+        assertThat(warmupPractice.array123(new int[]{1}), is(false));
+        assertThat(warmupPractice.array123(new int[]{}), is(false));
+    }
+
+    @Test
+    public void stringMatch() {
+        assertThat(warmupPractice.stringMatch("xxcaazz", "xxbaaz"), is(3));
+        assertThat(warmupPractice.stringMatch("abc", "abc"), is(2));
+        assertThat(warmupPractice.stringMatch("abc", "axc"), is(0));
+        assertThat(warmupPractice.stringMatch("hello", "he"), is(1));
+        assertThat(warmupPractice.stringMatch("he", "hello"), is(1));
+        assertThat(warmupPractice.stringMatch("h", "hello"), is(0));
+        assertThat(warmupPractice.stringMatch("", "hello"), is(0));
+        assertThat(warmupPractice.stringMatch("aabbccdd", "abbbxxd"), is(1));
+        assertThat(warmupPractice.stringMatch("aaxxaaxx", "iaxxai"), is(3));
+        assertThat(warmupPractice.stringMatch("iaxxai", "aaxxaaxx"), is(3));
+    }
+
+    @Test
+    public void stringX() {
+        assertThat(warmupPractice.stringX("xxHxix"),is("xHix"));
+        assertThat(warmupPractice.stringX("abxxxcd"),is("abcd"));
+        assertThat(warmupPractice.stringX("xabxxxcdx"),is("xabcdx"));
+        assertThat(warmupPractice.stringX("xKittenx"),is("xKittenx"));
+        assertThat(warmupPractice.stringX("Hello"),is("Hello"));
+        assertThat(warmupPractice.stringX("xx"),is("xx"));
+        assertThat(warmupPractice.stringX("x"),is("x"));
+        assertThat(warmupPractice.stringX(""),is(""));
+    }
+
+    @Test
+    public void altPairs() {
+        assertThat(warmupPractice.altPairs("kitten"), is("kien"));
+        assertThat(warmupPractice.altPairs("Chocolate"), is("Chole"));
+        assertThat(warmupPractice.altPairs("CodingHorror"), is("Congrr"));
+        assertThat(warmupPractice.altPairs("yak"), is("ya"));
+        assertThat(warmupPractice.altPairs("ya"), is("ya"));
+        assertThat(warmupPractice.altPairs("y"), is("y"));
+        assertThat(warmupPractice.altPairs(""), is(""));
+        assertThat(warmupPractice.altPairs("ThisThatTheOther"), is("ThThThth"));
+    }
+
+    @Test
+    public void stringYak() {
+        assertThat(warmupPractice.stringYak("yakpak"), is("pak"));
+        assertThat(warmupPractice.stringYak("pakyak"), is("pak"));
+        assertThat(warmupPractice.stringYak("yak123ya"), is("123ya"));
+        assertThat(warmupPractice.stringYak("yak"), is(""));
+        assertThat(warmupPractice.stringYak("yakxxxyak"), is("xxx"));
+        assertThat(warmupPractice.stringYak("HiyakHi"), is( "HiHi"));
+        assertThat(warmupPractice.stringYak("xxxyakyyyakzzz"), is("xxxyyzzz"	));
+    }
+
+    @Test
+    public void array667() {
+        assertThat(warmupPractice.array667(new int[]{6, 6, 2}), is(1));
+        assertThat(warmupPractice.array667(new int[]{6, 6, 2, 6}), is(1));
+        assertThat(warmupPractice.array667(new int[]{6, 7, 2, 6}), is(1));
+        assertThat(warmupPractice.array667(new int[]{6, 6, 2, 6, 7}), is(2));
+        assertThat(warmupPractice.array667(new int[]{1, 6, 3}), is(0));
+        assertThat(warmupPractice.array667(new int[]{6, 1}), is(0));
+        assertThat(warmupPractice.array667(new int[]{}), is(0));
+        assertThat(warmupPractice.array667(new int[]{3, 6, 7, 6}), is(1));
+        assertThat(warmupPractice.array667(new int[]{3, 6, 6, 7}), is(2));
+        assertThat(warmupPractice.array667(new int[]{6, 3, 6, 6}), is(1));
+        assertThat(warmupPractice.array667(new int[]{6, 7, 6, 6}), is(2));
+        assertThat(warmupPractice.array667(new int[]{1, 2, 3, 5, 6}), is(0));
+        assertThat(warmupPractice.array667(new int[]{1, 2, 3, 6, 6}), is(1));
+    }
+
+    @Test
+    public void noTriples() {
+        assertThat(warmupPractice.noTriples(new int[]{1, 1, 2, 2, 1}), is(true));
+        assertThat(warmupPractice.noTriples(new int[]{1, 1, 2, 2, 2, 1}), is(false));
+        assertThat(warmupPractice.noTriples(new int[]{1, 1, 1, 2, 2, 2, 1}), is(false));
+        assertThat(warmupPractice.noTriples(new int[]{1, 1, 2, 2, 1, 2, 1}), is(true));
+        assertThat(warmupPractice.noTriples(new int[]{1, 2, 1}), is(true));
+        assertThat(warmupPractice.noTriples(new int[]{1, 1, 1}), is(false));
+        assertThat(warmupPractice.noTriples(new int[]{1, 1}), is(true));
+        assertThat(warmupPractice.noTriples(new int[]{1}), is(true));
+        assertThat(warmupPractice.noTriples(new int[]{}), is(true));
+
+    }
+
+    @Test
+    public void has271() {
+        assertThat(warmupPractice.has271(new int[]{1, 2, 7, 1}), is(true));
+        assertThat(warmupPractice.has271(new int[]{1, 2, 8, 1}), is(false));
+        assertThat(warmupPractice.has271(new int[]{2, 7, 1}), is(true));
+        assertThat(warmupPractice.has271(new int[]{3, 8, 2}), is(true));
+        assertThat(warmupPractice.has271(new int[]{2, 7, 3}), is(true));
+        assertThat(warmupPractice.has271(new int[]{2, 7, 4}), is(false));
+        assertThat(warmupPractice.has271(new int[]{2, 7, -1}), is(true));
+        assertThat(warmupPractice.has271(new int[]{2, 7, -2}), is(false));
+        assertThat(warmupPractice.has271(new int[]{4, 5, 3, 8, 0}), is(true));
+        assertThat(warmupPractice.has271(new int[]{2, 7, 5, 10, 4}), is(true));
+        assertThat(warmupPractice.has271(new int[]{2, 7, 5, 10, 1}), is(false));
+        assertThat(warmupPractice.has271(new int[]{2, 7, -2, 4, 10, 2}), is(false));
+        assertThat(warmupPractice.has271(new int[]{1, 1, 4, 9, 0}), is(false));
+        assertThat(warmupPractice.has271(new int[]{1, 1, 4, 9, 4, 9, 2}), is(true));
+    }
+
+
 }
